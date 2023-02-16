da86aba63ec3ed092dd533ef04e83c77f47b02ae
scffs <67464545+scffs@users.noreply.github.com>
2023-02-16 21:25:23 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Clock from './components/Clock/Clock.jsx'
+function App() {
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Clock = () => {
+    return (
+        <div style={{color: 'white'}}>
+            Clock
+        </div>
+    );
+};
+
+export default Clock
